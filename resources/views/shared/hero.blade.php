d84d24420dcebd013c3b099ab3b5ba67dd0cee5d
roomdada <dsieroger@gmail.com>
2024-02-29 18:49:43 +0100

    wip

--- resources/views/shared/hero.blade.php
+++ resources/views/shared/hero.blade.php
@@ -1,28 +1,46 @@
-<section class="bg-white dark:bg-gray-900">
-  <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-      <div class="mr-auto place-self-center  lg:col-span-7">
-          <h1
-              class="max-w-2xl mb-4 text-2xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-5xl dark:text-white">
-              Découvrez, partagez et vivez des expériences uniques près de chez vous.</h1>
-          <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-
-          </p>
-          <a href="#"
-              class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-              Retrouver des evenements
-              <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd"
-                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                      clip-rule="evenodd"></path>
-              </svg>
-          </a>
-          <a href="#"
-              class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-              Publier un evenement
-          </a>
-      </div>
-      <div class="lg:mt-0 lg:col-span-5 lg:flex">
-          <img src="{{ asset('images/hero-img.jpg') }}" alt="mockup">
-      </div>
-  </div>
+<section class="w-full relative bg-white">
+    <svg class="w-64 fill-current text-gray-200 absolute left-0 rotate-45 transform top-1/2 2xl:opacity-50 lg:opacity-20 lg:block hidden -translate-y-1/2 -mt-20 -translate-x-1/2"
+        viewBox="0 0 280 364" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M266 84h-91c-7 0-14 0-14-7 0-14 14-21 14-42S161 0 140 0s-35 14-35 35 14 28 14 42c0 7-7 7-14 7H14C0 84 0 84 0 98v91c0 7 0 14 7 14 14 0 21-14 42-14s35 14 35 35-14 35-35 35-28-14-42-14c-7 0-7 7-7 14v91c0 14 0 14 14 14h91c7 0 14 0 14-7 0-14-14-21-14-42s14-35 35-35 35 14 35 35-14 28-14 42c0 7 7 7 14 7h91c14 0 14 0 14-14v-91c0-7 0-14-7-14-14 0-21 14-42 14s-35-14-35-35 14-35 35-35 28 14 42 14c7 0 7-7 7-14V98c0-14 0-14-14-14Z" />
+    </svg>
+    <svg class="w-full max-w-sm fill-current text-gray-200 absolute right-0 transform top-1/2 2xl:opacity-50 lg:opacity-20 lg:block hidden -translate-y-1/2 -mt-20 translate-x-1/2"
+        viewBox="0 0 420 402" height="402" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M1.94 169.17s61.531-81.707 97.859-63.734c36.348 17.973-7.246 48.914 27.965 65.082 35.191 16.172 88.34-4.008 76.578-50.734s-32.656-13.578-48.965-38.641C139.033 56.083 235.615.819 235.615.819s58.816 116.39 75.531 85.94c20.246-36.835 55.859-74.636 92.836-17.937 0 0 31.727 42.211 4.305 60.094-27.422 17.887-51.047 7.754-61.355 27.176-10.254 19.461 66.219 71.629 66.219 71.629s-52.273 84.191-86.203 62.109c-33.934-22.102 6.195-58.887-45.57-69.824-22.539-4.008-60.164 9.433-66.797 41.195-2.274 47.406 28.07 35.367 48.844 60.883 20.844 25.461-78.363 79.117-78.363 79.117s-27.719-68.723-66.691-81.426c-38.973-12.758-15.629 37.555-51.047 44.152-35.422 6.93-84.281-41.93-60.551-83.754 23.78-41.93 57.328-.805 60.234-34.125 3.007-33.707-65.066-76.879-65.066-76.879l-.001.001Z" />
+    </svg>
+    <div class="flex max-w-7xl lg:flex-row flex-col px-10 relative mx-auto py-12 lg:py-32">
+        <div class="lg:w-1/2 w-full lg:max-w-none max-w-2xl mx-auto relative flex items-start justify-center flex-col">
+            <div class="relative">
+                <div class="relative flex -rotate-2 text-black px-3 mb-10 md:mb-4 py-1.5 uppercase font-bold text-xs">
+                    <span class="absolute inset-0 rounded bg-gray-300 z-20"></span>
+                    <span class="w-full h-full absolute rounded right-0 -mr-0.5 top-0 mt-0.5 bg-gray-400"></span>
+                    <span class="relative z-20">{{ __('Les meilleurs evenements du moment') }}</span>
+                </div>
+            </div>
+            <h1 class="text-5xl md:text-6xl lg:text-7xl font-extrabold mb-6 relative">
+                Expolorer
+                <span class="relative inline-block">
+                    <span class="relative z-10 p-6">les</span>
+                    <svg class="w-full fill-current rotate-12 -mt-4 text-yellow-300 absolute right-0 top-0"
+                        viewBox="0 0 386 189" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M91.315 32.349c-43.9 18.5-66.3 38.1-72.8 63.8-1.3 5.3-2.9 8.5-6.1 12.5-19.2 24.1-15.9 50.4 8.3 66.5 25.5 16.9 97.2 17.9 174 2.4 11-2.2 28.8-5.4 39.5-7 133.3-20.1 177.8-79 137.4-119.6-24.6-24.8-60.9-35.4-121.4-35.8-57.6-.3-134.1 23.6-189.2 56.5-14.4 8.6-16.1 5.2-11.9.8 14.8-15.6 54.7-35.9 96.4-48.2 37.2-11 54.4-15.5 82.3-15.7 10.9-.1 17.2-7.6.4-8.5-17.8-.8-89.2 12.2-136.9 32.3Zm268.1 74.9c-17.7 22.2-58.5 38.7-124.6 50.2-220.9 38.4-203.5-44.7-203.5-44.7.2-.1 69.7-68.8 206.5-85.5 67.3-8.3 174.6 13.5 121.6 80Zm-263.8 67.4c22.8 3.1-4 3.7-37.8.9-63-5.2-44.8-41.5-39-47.5 18.8 27.1 44.9 42.3 76.8 46.6Z" />
+                    </svg>
+                </span>
+                meilleurs evenements
+            </h1>
+            <div class="relative">
+            </div>
+            <p class="text-gray-500 text-xl lg:max-w-md">Larapass : Votre portail vers une multitude d'expériences inédites, à portée de main. Découvrez, réservez et partagez des moments mémorables, le tout depuis une seule plateforme.</p>
+            <div class="flex md:flex-row flex-col mt-7 md:w-auto w-full md:space-y-0 space-y-5 md:space-x-2">
+                <a href="#_"
+                    class="px-6 py-3 md:w-auto w-full text-center text-lg bg-blue-600 text-white rounded font-bold">Decouvrir maintenant</a>
+                <a href="#_" class="px-6 py-3 text-lg bg-gray-200 text-center text-gray-700 rounded font-bold">Publier un evenement</a>
+            </div>
+        </div>
+        <div class="lg:w-1/2 w-full lg:max-w-none max-w-2xl mx-auto lg:pt-0 pt-16 lg:pl-10 xl:pl-0">
+            <img src="https://cdn.devdojo.com/images/march2022/hero-01.png">
+        </div>
+    </div>
 </section>
